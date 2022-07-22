e2651964bff1f51c5ecbddc4f12168430da0cda4
Andreas Müller <schnitzeltony@gmail.com>
2022-07-22 21:05:37 +0200

    Kernel-module: Make out-of-source-tree-build and clean work
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/client-lib/kmodulehelpers.cpp
+++ src/client-lib/kmodulehelpers.cpp
@@ -8,7 +8,7 @@ namespace KModuleHelpers {
 
 bool loadKernelModule()
 {
-    std::string cmd = std::string("insmod ") + std::string(CNC_KMOD) + std::string(" 2>&1");
+    std::string cmd = std::string("insmod ") + std::string(CNC_KMODULE_PATH) + std::string(" 2>&1");
     FILE *fd = popen(cmd.c_str(), "r");
     char buf[256];
     return fread(buf, 1, sizeof(buf), fd) == 0;
@@ -23,7 +23,7 @@ bool checkModuleLoaded()
 
 bool removeKernelModule()
 {
-    std::string cmd = std::string("rmmod ") + std::string(CNC_KMOD) + std::string(" 2>&1");
+    std::string cmd = std::string("rmmod ") + std::string(CNC_KMODULE_PATH) + std::string(" 2>&1");
     FILE *fd = popen(cmd.c_str(), "r");
     char buf[256];
     return fread(buf, 1, sizeof(buf), fd) == 0;
